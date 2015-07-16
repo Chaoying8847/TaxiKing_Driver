52d8690980a080d38dcbead5dd03a70d9688e059
Chaoying8847 <chaoying8847@outlook.com>
2015-07-17 02:52:44 +0800

    parse notification settings added

--- src/com/taxiking/driver/TaxiKing.java
+++ src/com/taxiking/driver/TaxiKing.java
@@ -2,6 +2,8 @@ package com.taxiking.driver;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.PushService;
 import com.taxiking.driver.service.GPSTracker;
 
 public class TaxiKing extends Application {
@@ -10,6 +12,10 @@ public class TaxiKing extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		
+		Parse.initialize(this, "9wvAGHzEjnKQ7KjG8mJjLFwQYDlB1ityliXxGAl7", "4p6xJzoF9EPAlRGYhjP3zuJKMNbsbxcyBDLcgNA9");
+		PushService.setDefaultPushCallback(this, MainActivity.class);
+		
 		gpsTracker = new GPSTracker(this);
 		gpsTracker.startTracking();
 		
